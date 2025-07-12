8290a6726080a847a46d4af38250f8d6732fe4a4
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-12 11:37:40 +0900

    JIT를 사용하는 pypy로 제출

--- 9663.py
+++ 9663.py
@@ -1,29 +1,3 @@
 import sys,math
 input= sys.stdin.readline
 
-N= int(input())
-
-cols=[0] * N
-ans=0
-
-
-def check(r,c):
-    for i in range(0,r):
-        if(abs(r-i)==abs(cols[i]-c)): return False
-        if(cols[i]==c): return False
-    return True
-
-    
-def bt(idx):
-    global ans
-    if(idx==N):
-        ans+=1
-        return
-    
-    for i in range(0,N):
-        if(check(idx,i)):
-            cols[idx]=i
-            bt(idx+1)
-
-bt(0)
-print(ans)
