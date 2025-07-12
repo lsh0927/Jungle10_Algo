df6e777e3c8506329187e0da9d9bfc10d3a645c5
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-12 11:51:53 +0900

    백트래킹
    한번더 복습 필요

--- 10819.py
+++ 10819.py
@@ -2,24 +2,31 @@ import sys,math
 input= sys.stdin.readline
 
 N= int(input())
-li= list(map(int, input().split()))
+li=list(map(int,input().split()))
+
 
 max_val=0
-def bt(idx,sum,arr,used):
+
+def bt(idx,sum,arr,visited):
     global max_val
-    if idx==N:
-        max_val=max(max_val,sum)
+    
+    if(idx==N):
+        max_val=max(sum,max_val)
         return
+    
 
     for i in range(N):
-        if not used[i]:
-            used[i] = True
-            arr[idx] = li[i]
-            new_sum = sum
-            if idx > 0:
-                new_sum += abs(arr[idx-1] - arr[idx])
-            bt(idx+1,new_sum,arr,used)
-            used[i] = False
+        if not visited[i]:
+            visited[i]=True
+            arr[idx]=li[i]
 
-bt(0,0,[0]*N,[False]*N)
+            newSum= sum
+            
+            if idx>0:
+                newSum+=abs(arr[idx-1]-arr[idx])
+            bt(idx+1,newSum,arr,visited)
+            visited[i]=False
+
+
+bt(0,0, [0] * N, [False] *N)
 print(max_val)
