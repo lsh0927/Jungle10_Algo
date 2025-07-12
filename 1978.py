acdcfe3a205b538cece0a5899478eed7c53b2f5b
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-12 10:11:37 +0900

    OK

--- 1978.py
+++ 1978.py
@@ -1,29 +1,21 @@
 import sys,math
 input= sys.stdin.readline
 
-N= int(input())
-li= list(map(int,input().split()))
 
+N=int(input())
+li=list(map(int, input().split()))
 
-x=max(li)
-
+max_val= max(li)
+isPrime= [True]* (max_val+1)
+isPrime[0]=isPrime[1]=False
 cnt=0
 
-isPrime=[True]*(x+1)
-isPrime[0]=isPrime[1]=False
-
-#에라토스테네스의 체
-def ert():
-    for i in range(2, int(math.sqrt(x))+1):
-        if isPrime[i]:
-            for j in range(i*i,x+1, i):
-                isPrime[j]=False
-    
-ert()
+for i in range(2,int(math.sqrt(max_val))+1):
+    for j in range(i*i,max_val,i):
+        isPrime[j]=False
 
 for val in li:
     if isPrime[val]:
         cnt+=1
 
 print(cnt)
-
