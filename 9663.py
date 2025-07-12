1785e5804fd1bf28656839cb58c21dde0b61e0d9
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-12 11:41:07 +0900

    JIT 사용하는 pypy 로 제출
    visited는 열 체크를 위한 수단이나 사용시 2배이상 빠름

--- 9663.py
+++ 9663.py
@@ -1,3 +1,28 @@
 import sys,math
 input= sys.stdin.readline
 
+N= int(input())
+cols = [0] * N  
+cnt=0
+
+def check(r,c):
+    for i in range(r):
+        if cols[i]==c: return False
+        if abs(i-r)==abs(cols[i]-c) : return False
+    return True
+
+
+def bt(idx):
+    global cnt
+    if idx==N:
+        cnt+=1
+        return
+
+    for i in range(N):
+        # 행과 열 체크
+        if(check(idx,i)):
+            cols[idx]=i
+            bt(idx+1)
+
+bt(0)
+print(cnt)
