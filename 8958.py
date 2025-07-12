c2c1e276b310e271a859932a2cb52d522b3bdc21
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-12 10:56:44 +0900

    list없이 구현

--- 8958.py
+++ 8958.py
@@ -1,42 +1,22 @@
 import sys,math
 input= sys.stdin.readline
 
-T= int(input())
+N= int(input())
 
-# for i in range(1,T+1):
-#     sum=0
-#     list=[]
-#     a= input()
-#     for j in range(0,len(a)):
-#         if(j==0 and a[0]=='O'):
-#             list.append(1)
-#         elif(j==0 and a[0]=='X'):
-#             list.append(0)
-#         elif(a[j]=='O'):
-#             list.append(list[j-1]+1)
-#         else:
-#             list.append(0)
+for _ in range(N):
+    str= input()
+
+    score=0
+    cons=0
+    if str[0]=='O':
+        score+=1
+        cons+=1
     
-#     for j in range(0,len(a)):
-#         sum+=list[j]
-    
-#     print(sum)
-
-
-
-# _는 사용하지 않는 변수
-for _ in range(T):
-    # 0~T-1
-    total=0
-    consecusive=0
-
-    a= input().strip()
-
-    for char in a:
-        if char == 'O':
-            consecusive+=1
-            total+=consecusive
+    for i in range(1,len(str)):
+        if(str[i]=='O'):
+            cons+=1
+            score+=cons
         else:
-            consecusive=0
+            cons=0
 
-    print(total)
+    print(score)
