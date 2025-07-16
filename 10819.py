55ed189c5017e993c5b74a24214e9c210d8fd67d
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-16 10:00:39 +0900

    배치기반 접근 & 경로기반 접근

--- 10819.py
+++ 10819.py
@@ -30,3 +30,36 @@ def bt(idx,sum,arr,visited):
 
 bt(0,0, [0] * N, [False] *N)
 print(max_val)
+
+#위는 배치기반 접근
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+li = list(map(int, input().split()))
+
+max_val = 0
+
+def bt(now, depth, sum):
+    global max_val
+    if depth == N:
+        max_val = max(max_val, sum)
+        return
+
+    for next in range(N):  
+        if not visited[next]:
+            visited[next] = True
+            diff = abs(li[now] - li[next])    
+            bt(next, depth + 1, sum + diff)
+            visited[next] = False
+
+for i in range(N):
+    visited = [False] * N
+    visited[i] = True
+    bt(i, 1, 0)  
+
+print(max_val)
+
+# 경로 기반도 있음
+# IterTools.permutation
