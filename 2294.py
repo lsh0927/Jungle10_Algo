d11e476f1f089d9e64f6c1825ac0d5ec471074e5
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-26 17:32:50 +0900

    bfs 이용 풀이
    vistied 활용

--- 2294.py
+++ 2294.py
@@ -1,7 +1,32 @@
-import sys,math
-input= sys.stdin.readline
+import sys
 from collections import deque
-import heapq as hq
 
+input = sys.stdin.readline
 
-n,k= map(int,input())
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+   coins.append(int(input()))
+
+coins = list(set(coins))
+coins.sort()
+
+queue = deque([(0, 0)]) 
+visited = [False] * (k + 1)
+visited[0] = True
+
+while queue:
+   current_amount, coin_count = queue.popleft()
+   
+   if current_amount == k:
+       print(coin_count)
+       exit()
+   
+   for coin in coins:
+       next_amount = current_amount + coin
+       
+       if next_amount <= k and not visited[next_amount]:
+           visited[next_amount] = True
+           queue.append((next_amount, coin_count + 1))
+
+print(-1)
