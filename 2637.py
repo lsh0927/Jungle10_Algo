c2154cab2503206c2c8078237cc0bb27e3f517a1
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-26 21:01:52 +0900

    위상정렬
    count(0)의 이미 복습

---
+++ 2637.py
@@ -0,0 +1,49 @@
+import sys,math
+input= sys.stdin.readline
+from collections import deque
+import heapq as hq
+
+n=int(input())
+m=int(input())
+
+q= deque()
+degree= [0] * (n+1)
+
+seq= [[] for _ in range(n+1)]
+needs= [ [0] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a,b,c= map(int, input().split())
+    seq[b].append((a,c))
+    degree[a]+=1
+
+for i in range(1,n+1):
+    if degree[i]==0:
+        q.append(i)
+
+
+while q:
+    cur = q.popleft()   
+    
+    for next, next_need in seq[cur]:
+
+        # 기본제품
+        if needs[cur].count(0) ==n+1:
+            needs[next][cur] += next_need
+
+        # 중간제품
+        else:
+            for i in range(1,n+1):
+                needs[next][i]+=needs[cur][i] * next_need
+
+        # 차수 -1
+        degree[next] -= 1
+        if degree[next]==0:
+            #차수 0이면 큐에 넣음
+            q.append(next)
+
+for x in enumerate(needs[n]):
+    if x[1]>0:
+        print(*x)
+
+print(needs)
