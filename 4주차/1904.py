9494f7d88e8ff430cb276d3405f82004fb240309
lsheon0927@naver.com <lsheon0927@naver.com>
2025-07-31 23:15:54 +0900

    기본 문제

---
+++ 4주차/1904.py
@@ -0,0 +1,21 @@
+import sys
+input= sys.stdin.readline
+
+
+N=int(input())
+
+dp= [0]*(N+1)
+
+dp[0]=0
+dp[1]=1
+
+if N>=2:
+    dp[2]=2
+
+
+
+
+for i in range(3, N+1):
+    dp[i]=(dp[i-1]+dp[i-2])%15746
+
+print(dp[N])
