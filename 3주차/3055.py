096e83c87993bd15a2906bb67bb074df2d160ea5
lsheon0927@naver.com <lsheon0927@naver.com>
2025-09-22 11:35:21 +0900

    0922

--- 3주차/3055.py
+++ 3주차/3055.py
@@ -48,68 +48,160 @@
 
 # print("KAKTUS")
 
+# import sys
+# input= sys.stdin.readline
+# from collections import deque
+
+
+# R,C= map(int,input().split())
+# li=[]
+# for _ in range(R):
+#     li.append(list(input().strip()))
+
+
+# q=deque()
+
+# dx=[1,0,-1,0]
+# dy=[0,1,0,-1]
+
+
+# visit= [[False] * C  for _ in range(R)]
+# # 물 먼저 큐에 넣기
+# for i in range(R):
+#     for j in range(C):
+#         if(li[i][j]=='*'):
+#             q.appendleft((i,j,0))
+#         if(li[i][j]=="S"):
+#             q.append((i,j,0))
+#             visit[i][j] = True
+
+
+# while q:
+#     cx,cy,ctime= q.popleft()
+    
+#     if(li[cx][cy]=='D'):
+#         print(ctime)
+#         exit()
+
+#     # 큐의 순서대로 퍼뜨리기
+#     for i in range(4):
+#         nx= cx+dx[i]
+#         ny= cy+dy[i]
+
+#         if(0<=nx<R and 0<=ny<C and not visit[nx][ny] and li[nx][ny]=='.'):
+#             if li[cx][cy]=='*':
+#                 visit[nx][ny]=True
+#                 li[nx][ny]='*'
+#             elif li[cx][cy]=='S':
+#                 visit[nx][ny]=True
+#                 li[nx][ny]='S'
+#             q.append((nx,ny,ctime+1))
+
+# print("KAKTUS")
+
+
+# import sys
+# input = sys.stdin.readline
+# from collections import deque
+
+# R, C = map(int, input().split())
+# li = []
+# for _ in range(R):
+#     li.append(list(input().strip()))
+
+# q = deque()
+# dx = [1, 0, -1, 0]
+# dy = [0, 1, 0, -1]
+# visit = [[False] * C for _ in range(R)]
+
+# # 물 먼저 큐에 넣기 (appendleft로 앞쪽에)
+# for i in range(R):
+#     for j in range(C):
+#         if li[i][j] == '*':
+#             q.appendleft((i, j, 0, '*'))  # 타입 정보 추가
+
+# # 고슴도치 나중에 큐에 넣기 (append로 뒤쪽에)
+# for i in range(R):
+#     for j in range(C):
+#         if li[i][j] == 'S':
+#             q.append((i, j, 0, 'S'))  # 타입 정보 추가
+#             visit[i][j] = True
+
+# while q:
+#     cx, cy, ctime, cell_type = q.popleft()
+    
+#     # 고슴도치가 목적지에 도달했는지 체크
+#     if cell_type == 'S' and li[cx][cy] == 'D':
+#         print(ctime)
+#         exit()
+
+#     # 4방향으로 퍼뜨리기/이동하기
+#     for i in range(4):
+#         nx = cx + dx[i]
+#         ny = cy + dy[i]
+
+#         if 0 <= nx < R and 0 <= ny < C:
+#             if cell_type == '*':  # 물 처리
+#                 if li[nx][ny] == '.':  # 물은 빈 공간으로만 퍼짐
+#                     li[nx][ny] = '*'
+#                     q.append((nx, ny, ctime + 1, '*'))
+                    
+#             else:  # 고슴도치 처리 (cell_type == 'S')
+#                 if not visit[nx][ny] and (li[nx][ny] == '.' or li[nx][ny] == 'D'):
+#                     visit[nx][ny] = True
+#                     q.append((nx, ny, ctime + 1, 'S'))
+
+# print("KAKTUS")
+
+import sys
+input = sys.stdin.readline
 from collections import deque
 
-R,C= map(int,input().split())
-grid=[]
-
+R, C = map(int, input().split())
+li = []
 for _ in range(R):
-    grid.append(list(input().strip()))
+    li.append(list(input().strip()))
 
-dx=[0,0,-1,1]
-dy=[-1,1,0,0]
+q = deque()
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+visit = [[False] * C for _ in range(R)]
 
-water_q= deque()
-hedgehog_q= deque()
-
-#물과 고슴도치 위치 찾기
-
+# 물 먼저 큐에 넣기
 for i in range(R):
     for j in range(C):
-        if grid[i][j]=="*":
-            water_q.append((i,j))
-        elif grid[i][j]=="S":
-            hedgehog_q.append((i,j))
+        if li[i][j] == '*':
+            q.appendleft((i, j, 0))
+        if li[i][j] == "S":
+            q.append((i, j, 0))
+            visit[i][j] = True
 
-visit= [[False] * C for _ in range(R)]
-
-# 고습도치 시작점 방문처리
-for i in range(R):
-    for j in range(C):
-        if grid[i][j]=="S":
-            visit[i][j]=True
-            break
-
-time=0
-
-# 조건은 고슴도치가 움직일 수 있을 때
-while hedgehog_q:
-    time+=1
-
-    #1단계: 물 먼저 퍼뜨리기
-    water_size= len(water_q)
-    for _ in range(water_size):
-        cx,cy= water_q.popleft()
-
-        for i in range(4):
-            nx,ny=cx+dx[i], cy+dy[i]
-            if 0 <= nx < R and 0 <= ny < C and grid[nx][ny] == '.':
-                grid[nx][ny]="*"
-                water_q.append((nx,ny))
-
-    hedgehog_size= len(hedgehog_q)
-    for _ in range(hedgehog_size):
-        cx,cy= hedgehog_q.popleft()
-
-        for i in range(4):
-            nx, ny= cx+dx[i], cy+dy[i]
-            if 0 <= nx < R and 0 <= ny < C and not visit[nx][ny]:
-                if grid[nx][ny]=="D":
-                    print(time)
-                    exit()
-                elif grid[nx][ny]==".":
-                    visit[nx][ny]=True
-                    hedgehog_q.append((nx,ny))
+while q:
+    cx, cy, ctime = q.popleft()
     
+    # 고슴도치가 D에 도달했는지 체크 (이동하기 전에 체크)
+    if li[cx][cy] == 'D':
+        print(ctime)
+        exit()
+
+    # 4방향으로 퍼뜨리기
+    for i in range(4):
+        nx = cx + dx[i]
+        ny = cy + dy[i]
+
+        if 0 <= nx < R and 0 <= ny < C and not visit[nx][ny]:
+            if li[cx][cy] == '*':  # 물 처리
+                if li[nx][ny] == '.':  # 물은 빈 공간으로만 퍼짐
+                    visit[nx][ny] = True
+                    li[nx][ny] = '*'
+                    q.append((nx, ny, ctime + 1))
+                    
+            elif li[cx][cy] == 'S':  # 고슴도치 처리
+                if li[nx][ny] == '.' or li[nx][ny] == 'D':
+                    visit[nx][ny] = True
+                    if li[nx][ny] == '.':  # 빈 공간일 때만 S로 변경
+                        li[nx][ny] = 'S'
+                    # D일 때는 격자를 수정하지 않음!
+                    q.append((nx, ny, ctime + 1))
 
 print("KAKTUS")
